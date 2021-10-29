a3099db70d1d6395b4d2c107d2358740fb2eef0a
liomes <76909087+liomes@users.noreply.github.com>
2021-10-29 15:00:26 +0800

    删除文件

--- day04/StartData.py
+++
@@ -1,7 +0,0 @@
-class StartData:
-    Ok_data = [{"user": "linhaiwang", "pws": "Lhw04030601", "expect": "Student Login"},
-               {"user": "linhaiwang", "pws": "Lhw04030601", "expect": "Student Login"},
-               {"user": "lhw", "pws": "Lhw", "expect": "Student Login"}
-               ]
-    No_data = [{"user": "linhaiwang1", "pws": "Lhw04030601", "expect": "登录名或手机不能为空!别瞎弄!"}]
-
